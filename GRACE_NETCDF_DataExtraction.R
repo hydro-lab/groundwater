03c99c72de2ece1ce82e1c9b4918e5b2394aca82
GabiZuccolotto <78925157+GabiZuccolotto@users.noreply.github.com>
2022-01-20 19:32:39 -0500

    Update GRACE_NETCDF_DataExtraction.R

--- GRACE_NETCDF_DataExtraction.R
+++ GRACE_NETCDF_DataExtraction.R
@@ -27,8 +27,10 @@ WET <- ncvar_get(GRACE, "lwe_thickness")
 D <- as.Date(Days_Since_2002_01_01, origin = '2002-01-01')
 Date <- as_date(parse_date_time(D,"ymd"))
 
-#Create new data frame with only WET values from lon: 33.8 (cell index 68), lat -24.8 (cell index 131)
+#Create new data frame with only WET values from lon and lat location, in this example: lon 33.8 (cell index 68), lat -24.8 (cell index 131)
+#Can use Panoply Data Viewer to view water equivalent thickness layer and determine cell indices for longitude and latitude
 Water_Equiv_Thickness_cm <- WET[68, 131, ]
+#The third dimension is time and is left intentionally blank so that values across entire time-series are collected
 
 #Create data frame with columns for time, dates and WET values
 data.frame(Days_Since_2002_01_01,Date,Water_Equiv_Thickness_cm) -> WET_XaiXai_TS
