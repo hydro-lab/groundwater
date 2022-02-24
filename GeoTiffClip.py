0698c5367a5b8ee1e1651ad72afa5b3bc41b5757
GabiZuccolotto <78925157+GabiZuccolotto@users.noreply.github.com>
2022-02-24 12:27:52 -0500

    Add files via upload
    HYP3 images that have been processed using InSAR gamma (see ASF vertex documentation for details) contain multiple .tif files. Each of these .tif files have files have to be cropped to the same geographic extent in order to be processed by MintPy. This code iterates through each image subfolder and clips each .tif file to the maximum shared extent of all the images, producing new clipped images that end with the extension "_clip.tif"

---
+++ GeoTiffClip.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import os
+import re
+import glob
+import zipfile
+import numpy as np
+# import hyp3_sdk # do not need because images are already downloaded
+from pathlib import Path
+from osgeo import gdal
+from mintpy import view, tsview
+
+
+fnames = glob.glob(os.path.join('/Volumes/hydro3-raid/Sentinel_Imagery/ASFXaiXai/hype', '*/*.tif'))
+
+# determine the smallest area covered by all input files
+corners = [gdal.Info(f, format='json')['cornerCoordinates'] for f in fnames]
+ulx = max(corner['upperLeft'][0] for corner in corners)
+uly = min(corner['upperLeft'][1] for corner in corners)
+lrx = min(corner['lowerRight'][0] for corner in corners)
+lry = max(corner['lowerRight'][1] for corner in corners)
+
+# subset all input files to these common coordinates
+for fname in fnames:
+    fname_out = fname.replace('.tif', '_clip.tif')
+    gdal.Translate(destName=fname_out, srcDS=fname, projWin=[ulx, uly, lrx, lry])
